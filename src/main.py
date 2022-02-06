3be440ddc80a830ddb1b562ad5ca97e5547340f7
andrepetsch <56927999+andrepetsch@users.noreply.github.com>
2022-02-06 18:02:57 +0100

    processing config, draft for run

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
-import httplib, daemon, ipaddress
+import httplib
+import daemon
+import ipaddress
+import argparse
 
 class strato_ddns:
     def __init__(self):
@@ -8,12 +11,22 @@ class strato_ddns:
         self.login = ""
         self.password = ""
         self.domain = []
-        self.ipv4 = "web"
-        self.ipv6 = "web"
+        self.ipv4 = "" # web?
+
+        self.ipv4_dns = ""
+        self.ipv4_real = ""
+        self.ipv6 = "" # web?
+        self.ipv6_dns = ""
+        self.ipv6_real = ""
 
     def __init__(self, config_path = "./strato_ddns.conf"):
         self.__init__()
         self.read_config(config_path=config_path)
+
+        if self.login=="" or self.password=="" or self.domain == []:
+            raise Exception("Missing information (login/pwd/domain) in .conf")
+        if self.ipv4=="" or self.ipv6=="":
+            raise Exception("Missing information (IPv4/IPv6) in .conf")
 
     def read_config(self, config_path):
         f = open(config_path)
@@ -23,6 +36,9 @@ class strato_ddns:
                 l = l.strip()
                 if l.startswith('#'):
                     # exclude comments
+                    pass
+                elif l == "":
+                    # pass empty lines
                     pass
                 else:
                     c = l.split('=')
@@ -48,7 +64,6 @@ class strato_ddns:
                             # parses to ip, throws error if no ip given
                             ip = ipaddress.ip_address(value)
                             self.ipv4 = value                            
-                        
                     elif option == "ipv6":
                         value = str(value)
                         if value == "web":
@@ -57,10 +72,50 @@ class strato_ddns:
                             # parses to ip, throws error if no ip given
                             ip = ipaddress.ip_address(value)
                             self.ipv6 = value
+                    else:
+                        # unexpected arguments in configuration
+                        raise Exception("invalid configuration")
 
         except Exception as e:
             print("Could not process .conf: ", str(e))
+            exit()
+
+        finally:
+            f.close()
         
+    def run(self):
+        # form updatestring
+        
+        change = False
+
+        if self.ipv4 != "":
+            # TODO: lookup dns ipv4
+            if self.ipv4 == "web":
+                # TODO: lookup real ipv4
+                # TODO: write to self.ipv4_real
+                pass
+            else:
+                if self.ipv4 != self.ipv4_dns:
+                    self.ipv4_real=self.ipv4
+
+        # TODO: IPv6
+        # TODO: complete updatestring
+        # TODO: send updatestring
+        # TODO: log response
+            
+            
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser(description="Updates ip on strato dyndns")
+    parser.add_argument(
+        '--config',
+        '-c',
+        help="configuration file",
+        type=str,
+        default="../strato_ddns.conf",
+        required=False
+    )
+    args = parser.parse_args()
+
+    s = strato_ddns(args.config)
+    print(s)
